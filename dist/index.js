ec87970db1017d7a7f7d76a5f5a30d43eeb9b92f
yangtianyu <535173883@qq.com>
2022-01-03 10:07:56 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -19720,7 +19720,11 @@ octokit.rest.issues.create({
 
 function getTitle() {
   // UTC时间转化
-  return "【每日计划】" + " " + dayjs().add("8", "hour").format("YYYY-MM-DD");
+  return (
+    "【每日计划】" +
+    " " +
+    dayjs().add("8", "hour").format("YYYY-MM-DD HH:mm:ss")
+  );
 }
 
 })();
